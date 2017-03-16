66329452116c640fd9ed08095487c6744c15d373
Danilo Maia Rodrigues <zidalo@gmail.com>
2017-03-16 16:30:51 -0300

    Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,26 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+
+// AF2 Settings
+export const firebaseConfig = {
+  apiKey: "AIzaSyCjPc7mcLZwFys8ELBVqmR42KdsoudQYBA",
+  authDomain: "maismateriais-43677.firebaseapp.com",
+  databaseURL: "https://maismateriais-43677.firebaseio.com",
+  storageBucket: "maismateriais-43677.appspot.com",
+  messagingSenderId: "887829339418"
+};
+
 @NgModule({
   declarations: [
     MyApp,
     HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
